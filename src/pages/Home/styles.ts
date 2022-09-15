066b11e857d3ba88dbf4564e58af198c2e8a9b10
caio300 <caioqs.info@gmail.com>
2022-09-15 15:19:56 -0300

    Funcionalidades da aplicação - Interromper ciclo

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -40,14 +40,12 @@ const BaseInput = styled.input`
     color: ${(props) => props.theme['gray-500']};
   }
 `
-
 export const TaskInput = styled(BaseInput)`
   flex: 1;
   &::-webkit-calendar-picker-indicator {
     display: none !important;
   }
 `
-
 export const MinutesAmountInput = styled(BaseInput)`
   width: 4rem;
 `
@@ -72,7 +70,8 @@ export const Separator = styled.div`
   display: flex;
   justify-content: center;
 `
-export const StartCountdownButton = styled.button`
+
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -80,16 +79,27 @@ export const StartCountdownButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+  color: ${(props) => props.theme['gray-100']};
   gap: 0.5rem;
   font-weight: bold;
   cursor: pointer;
-  background: ${(props) => props.theme['green-500']};
-  color: ${(props) => props.theme['gray-100']};
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
   }
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
   &:not(:disabled):hover {
     background: ${(props) => props.theme['green-700']};
+  }
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['red-500']};
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
   }
 `
